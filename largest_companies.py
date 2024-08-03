c805b3f71457a41d09c2327a4289d58bfba5c51c
Thiago <tpgoncalvesc@gmail.com>
2024-08-03 15:08:24 -0300

    Updating parser on largest_companies.py

--- largest_companies.py
+++ largest_companies.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
 page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html')
+soup = BeautifulSoup(page.text, 'html.parser')
 
 table = soup.find_all('table')[1]
 world_titles = table.find_all('th')
