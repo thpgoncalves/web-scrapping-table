7475f393f21ef1b754cad2b16c81147bf941e99e
Thiago Goncalves <162183868+thpgoncalves@users.noreply.github.com>
2024-08-05 14:49:34 -0300

    Update file name in largest_companies.py

--- largest_companies.py
+++ largest_companies.py
@@ -21,4 +21,4 @@ for row in column_data[1:]:
   length = len(df)
   df.loc[length] = individual_row_data
 
-df.to_csv(r'C:\Users\user\Desktop\Companies.csv', index = False) #add the local that you want to save your file here, you got to name the file and use .csv at the end.
+df.to_csv('biggest_companies.csv', index = False) #add the local that you want to save your file here, you got to name the file and use .csv at the end you can save it in any paste that you want.
